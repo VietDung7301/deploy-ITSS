c9259cef9e6e768c8deaa3bf26a3f6d10c053b20
VietDung <vietdung.7301@gmail.com>
2023-12-20 01:29:42 +0700

    Add API get user's CV and API apply job

--- modules/user/controller.js
+++ modules/user/controller.js
@@ -3,7 +3,6 @@ const service = require("./service");
 exports.getUserCv = async (req, res) => {
 	try {
 		const user_id = req.user.id;
-		console.log('user_id', user_id);
 		const user_cv = await service.getUserCv(user_id);
 		res.status(200).json(user_cv)
 	} catch (err) {
