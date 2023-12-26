916a88e923f37ecbb5033efa70e2a919157f2006
VietDung <vietdung.7301@gmail.com>
2023-12-26 21:47:46 +0700

    Add API to check if user was applied a company

--- modules/company/route.js
+++ modules/company/route.js
@@ -4,5 +4,6 @@ const controller = require('./controller');
 const { auth } = require('../../middleware')
 
 router.get('/company/:company_id', auth, controller.getCompanyInformation);
+router.get('/company/:company_id/is_applied', auth, controller.checkApplied);
 
 module.exports = router;
